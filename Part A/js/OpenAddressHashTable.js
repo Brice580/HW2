d424f9c13bbe6c744ee36abdfe5f1bba7afa0429
Brice Joseph <bricejoseph@brices-mbp.myfiosgateway.com>
2022-06-08 12:01:51 -0400

    putValue

--- Part A/js/OpenAddressHashTable.js
+++ Part A/js/OpenAddressHashTable.js
@@ -53,11 +53,82 @@ export default class OpenAddressHashTable {
     // @todo - YOU MUST DEFINE THIS METHOD
     removeValue(key) {   
     }
+    rehash(oldHash, newHash, newItemObject){
+        for(let i =0; i< this.length/2; i++){
+            let index = this.hashCode(oldHash[i].key);
+            if(newHash[index] == null){
+                newHash[index] = oldHash[i];
+            }
+            else{
+                let z = index;
+                while(newHash[z] != null){
+                    z = (z+1) % this.length;
+                }
+                if(newHash[z] == null){
+                    newHash[z] = oldHash[i];
+                }
+            }
+        }
+        let index1 = this.hashCode(newItemObject.key);
+        if(newHash[index1] == null){
+            newHash[index1] = newItemObject;
+            this.size++;
+        }
+        else{
+            let a = index1;
+            while(newHash[a] !=null){
+
+                a = (a+1) % this.length;
+
+            }
+            if(newHash[a] == null){
+                newHash[a] = newItemObject;
+                this.size++;
+            }
+        }
+        oldHash = newHash;
+        
+    }    
 
     // @todo - YOU MUST DEFINE THIS METHOD
     putValue(key, item) {
 
+        let index = this.hashCode(key);
+
+        let newItem = new KeyValuePair(key, item);
+
+        for(let i =0; i<this.length; i++){
+            if(this.hashTable[i] != null){
+                if(this.hashTable[i].key == key){
+                    this.hashTable[i] = newItem;
+                    return;
+                }
+            }
+
+        }
+        if(this.size == this.length){
+            this.length = this.length*2;
+            let newHashTable = new Array(this.length);
+            this.rehash(this.hashTable,newHashTable,newItem);
+
+        }
+        if(this.hashTable[index] == null){
+            this.hashTable[index] = newItem;
+            this.size++;
+        }
+        else if(this.hashTable[index] != null && this.size != this.length){
+           let i = index;
+            while(this.hashTable[i] != null){
+                i = (i+1) % this.length;
+            }
+            if(this.hashTable[i] == null){
+                this.hashTable[i] = newItem;
+                this.size++;
+                return;
+            }
+        }
     }
+
     
     toString() {
         let text = "[\n";
